5c7d16c1c76e9a049ebd98b337fe397d33f67039
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-14 19:07:31 -0700

    Finished Guided Project

---
+++ HobbyTracker/HobbyTracker/Models/Friend.swift
@@ -0,0 +1,15 @@
+//
+//  Friend.swift
+//  HobbyTracker
+//
+//  Created by Richard Gibbs on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let hometown: String
+    let hobbies: [String]
+}
---
+++ HobbyTracker/HobbyTracker/View Controllers/FriendsListViewController.swift
@@ -0,0 +1,77 @@
+//
+//  FriendsListViewController.swift
+//  HobbyTracker
+//
+//  Created by Richard Gibbs on 5/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FriendsListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var friends: [Friend] = [Friend(name: "Bob", hometown: "Salt Lake City", hobbies: ["Reading"])]
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Tell the table view that we (the view controller) are going to be its data source (meaning, we will help it get the information it needs)
+        
+        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+    
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "ModalNewFriend" {
+            
+            let newFriendVC = segue.destination as? NewFriendViewController
+            
+            // "Hey newFriendVC, I'm the one to talk to in order to add a friend to the array"
+            newFriendVC?.delegate = self
+        }
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    
+
+}
+
+extension FriendsListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell", for: indexPath) as? FriendTableViewCell else {
+            fatalError("Cell identifier is wrong or the cell is not of expected type FriendTableViewCell")
+        }
+                                    // 0
+        let friend = friends[indexPath.row]
+        cell.nameLabel.text = friend.name
+        cell.hometownLabel.text = friend.hometown
+        cell.hobbiesLabel.text = "\(friend.hobbies.count) hobbies"
+        return cell
+    }
+    
+    
+}
+
+// "I will follow the rules to add a new friend!"
+extension FriendsListViewController: AddFriendDelegate {
+    func friendWasCreated(friend: Friend) {
+        friends.append(friend)
+        tableView.reloadData()
+        
+    }
+    
+    
+}
